b8371d7da29795eb166bf2a3d38094e8d85b1115
djore4 <joao4duarte@hotmail.com>
2026-03-14 15:57:21 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <DataProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/baviera-vision-pro">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/retails" element={<AppLayout><RetailsPage /></AppLayout>} />
