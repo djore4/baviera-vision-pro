774aa0302ed5fca17be15dab7c98714b21e56b7f
djore4 <joao4duarte@hotmail.com>
2026-04-14 21:17:36 +0100

    Add EscalaPage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ProducaoPage from "./pages/ProducaoPage";
 import CarteiraPage from "./pages/CarteiraPage";
 import PendentesPage from "./pages/PendentesPage";
 import DadosPage from "./pages/DadosPage";
+import EscalaPage from "./pages/EscalaPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -28,6 +29,7 @@ const App = () => (
             <Route path="/producao" element={<AppLayout><ProducaoPage /></AppLayout>} />
             <Route path="/carteira" element={<AppLayout><CarteiraPage /></AppLayout>} />
             <Route path="/pendentes" element={<AppLayout><PendentesPage /></AppLayout>} />
+            <Route path="/escala" element={<AppLayout><EscalaPage /></AppLayout>} />
             <Route path="/dados" element={<AppLayout><DadosPage /></AppLayout>} />
             <Route path="*" element={<NotFound />} />
           </Routes>
