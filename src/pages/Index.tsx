bbd20924bc95ca1f2dc16d2ccbc37820e282ec7c
djore4 <joao4duarte@hotmail.com>
2026-04-20 09:35:42 +0100

    Simplify Index component by removing useEffect

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,3 @@
-import { useEffect } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-
-const Index = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const redirect = params.get('redirect');
-    if (redirect) navigate(redirect, { replace: true });
-  }, []);
-  return <Navigate to="/retails" replace />;
-};
+import { Navigate } from 'react-router-dom';
+const Index = () => <Navigate to="/retails" replace />;
 export default Index;
